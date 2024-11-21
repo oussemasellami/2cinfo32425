e65c4b9ca7719272c50c0c60228e529dcbf8f929
oussema.sellami <49963438+oussemasellami@users.noreply.github.com>
2024-11-21 20:31:11 +0100

    crud

--- projectcrud/app.js
+++ projectcrud/app.js
@@ -1,9 +1,19 @@
-const http=require('http')
-const express=require('express')
-const userRouter=require('./routes/users')
+const http = require("http");
+const express = require("express");
+const userRouter = require("./routes/users");
+const mongo = require("mongoose");
+const db = require("./config/db.json");
+mongo
+  .connect(db.url)
+  .then(() => {
+    console.log("database connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-var app=express()
-
-app.use('/users',userRouter)
-const server=http.createServer(app,console.log('server run!!!'))
-server.listen(3000)
+var app = express();
+app.use(express.json())
+app.use("/users", userRouter);
+const server = http.createServer(app, console.log("server run!!!"));
+server.listen(3000);
--- projectcrud/routes/users.js
+++ projectcrud/routes/users.js
@@ -1,7 +1,86 @@
-const express=require('express')
-const router=express.Router()
-router.get('/',(req,res)=>{
-    res.send('hello')
-})
+const express = require("express");
+const router = express.Router();
+const User = require("../models/user");
+router.get("/", (req, res) => {
+  res.send("hello");
+});
 
-module.exports=router
+router.get("/add/:username/:email/:cin", (req, res) => {
+  try {
+    new User({
+      username: req.params.username,
+      email: req.params.email,
+      cin: req.params.cin,
+    }).save();
+    res.status(200).send("good added");
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.post("/add", (req, res) => {
+  try {
+    console.log(req.body);
+    const user = new User(req.body);
+    user.save();
+    res.status(200).json(user);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.get("/showusers", async (req, res) => {
+  try {
+    const user = await User.find();
+
+    res.status(200).json(user);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.get("/showusers/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    res.status(200).json(user);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.get("/showbyname/:username", async (req, res) => {
+  try {
+    const { name } = req.params.username;
+    console.log(name);
+    const user = await User.find(name);
+
+    res.status(200).json(user);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.delete("/delete/:id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+
+    res.status(200).json(user);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.put("/update/:id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+
+    res.status(200).json(user);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+module.exports = router;
